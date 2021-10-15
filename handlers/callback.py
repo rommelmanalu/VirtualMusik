f811897b3269896695a2dbb7f231934331c6d50c
rommelmanalu <90427348+rommelmanalu@users.noreply.github.com>
2021-10-16 01:59:14 +0700

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -48,7 +48,7 @@ async def cbstart(_, query: CallbackQuery):
                 ],
                 [
                     InlineKeyboardButton(
-                        "🌐 Source Code", url="https://github.com/Tonic990/VirtualMusik"
+                        "🌐 Source Code", url="https://github.com/rommelmanalu/VirtualMusik"
                     )
                 ],
             ]
